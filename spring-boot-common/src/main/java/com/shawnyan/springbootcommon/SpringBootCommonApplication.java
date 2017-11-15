6639ee2e4eb32d514b75bb480317389d1bf2afa5
shawnyan <shawn2016@aliyun.com>
2017-11-15 23:33:19 +0800

    spring-boot--druid

--- spring-boot-common/src/main/java/com/shawnyan/springbootcommon/SpringBootCommonApplication.java
+++ spring-boot-common/src/main/java/com/shawnyan/springbootcommon/SpringBootCommonApplication.java
@@ -3,6 +3,9 @@ package com.shawnyan.springbootcommon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shawnyan
+ */
 @SpringBootApplication
 public class SpringBootCommonApplication {
 
